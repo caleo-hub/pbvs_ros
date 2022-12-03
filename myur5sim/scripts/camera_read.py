a3c7e941b9376990cb3e13f81ef12758aee132e6
caleo-hub <caleomenesessantos@gmail.com>
2022-12-03 20:12:28 -0300

    script ler camera

---
+++ myur5sim/scripts/camera_read.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+import rospy
+import cv2
+
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+class camera_1:
+
+  def __init__(self):
+    self.image_sub = rospy.Subscriber("/myur5/camera1/image_raw", Image, self.callback)
+
+  def callback(self,data):
+    bridge = CvBridge()
+
+    try:
+      cv_image = bridge.imgmsg_to_cv2(data, desired_encoding="bgr8")
+    except CvBridgeError as e:
+      print(e)
+      rospy.logerr(e)
+    
+    image = cv_image
+    resized_image = cv2.resize(image, (360, 640)) 
+
+    cv2.imshow("Camera output normal", image)
+    #cv2.imshow("Camera output resized", resized_image)
+
+    cv2.waitKey(3)
+
+def main():
+  a = camera_1()
+  
+  try:
+    rospy.spin()
+  except KeyboardInterrupt:
+    rospy.loginfo("Shutting down")
+  
+  cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    rospy.init_node('camera_read', anonymous=False)
+    main()
